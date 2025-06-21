10fdf3bdf2b7911687d8aaa5cf8334498c8b6e99
iiiiin <keepa0041@naver.com>
2025-06-22 01:26:18 +0900

    fix: 오프라인 캐싱 오류 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,16 @@ export default defineConfig({
     react(),
     VitePWA({ 
       registerType: 'autoUpdate',
-      injectRegister: 'auto',
+      injectRegister: 'inline',
+      // public 디렉터리(root)에 있는 파일을 precache
+      includeAssets: [
+        'vite.svg',
+        'rewardicon.png'
+      ],
+      // 워크박스가 assets 폴더 안 svg도 잡게 glob 패턴 확장
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico}']
+      },
       manifest: {
         name: 'Reward Demo PWA',
         short_name: 'Reward',
